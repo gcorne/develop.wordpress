377c5ca230ccb99fa5dec1d5cbb6b4f2d06c215f
Gregory Cornelius <gcorne@gmail.com>
2014-02-19 18:29:19 -0500

    Allow [] in redirects. See #17052

--- tests/phpunit/tests/formatting/redirect.php
+++ tests/phpunit/tests/formatting/redirect.php
@@ -13,5 +13,11 @@ class Tests_Formatting_Redirect extends WP_UnitTestCase {
 		//Nesting checks
 		$this->assertEquals('http://example.com/watchthecarriagereturngo', wp_sanitize_redirect('http://example.com/watchthecarriagereturn%0%0ddgo'));
 		$this->assertEquals('http://example.com/watchthecarriagereturngo', wp_sanitize_redirect('http://example.com/watchthecarriagereturn%0%0DDgo'));
+
+		//IPv6 literals
+		$this->assertEquals('http://[::1]/wp-admin/install.php', wp_sanitize_redirect('http://[::1]/wp-admin/install.php'));
+		$this->assertEquals('http://[::1]/wp-admin/install.php', wp_sanitize_redirect('http://[::1]/wp-admin/install.php'));
+		$this->assertEquals('http://[2606:2800:220:6d:26bf:1447:1097:aa7]/', wp_sanitize_redirect('http://[2606:2800:220:6d:26bf:1447:1097:aa7]/'));
+
 	}
 }
